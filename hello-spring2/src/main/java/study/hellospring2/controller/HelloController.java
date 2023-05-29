7d13b8af1deaa7f77d178a29c6372e1ff685076d
w0nnie <kjw55523@gmail.com>
2023-05-29 16:08:52 +0900

    MVC와 템플릿 엔진

--- hello-spring2/src/main/java/study/hellospring2/controller/HelloController.java
+++ hello-spring2/src/main/java/study/hellospring2/controller/HelloController.java
@@ -3,7 +3,9 @@ package study.hellospring2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
