1e0a16f3c559bbfb77720bb0784cd847b12b6d11
w0nnie <kjw55523@gmail.com>
2023-06-05 11:29:26 +0900

    회원 웹 기능 - 등록 회원 웹 기능 - 조회

--- hello-spring2/src/main/java/study/hellospring2/controller/MemberController.java
+++ hello-spring2/src/main/java/study/hellospring2/controller/MemberController.java
@@ -3,7 +3,14 @@ package study.hellospring2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.hellospring2.domain.Member;
+import study.hellospring2.domain.MemberForm;
 import study.hellospring2.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,5 +20,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberSerivce) {
         this.memberSerivce = memberSerivce;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @GetMapping("/members")
+    public String memberList(Model model) {
+        //memberService finMembers 메서드 조회
+        List<Member> members = memberSerivce.finMembers();
+        model.addAttribute("members" , members);
+        return "members/memberList";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        memberSerivce.join(member);
+
+        return "redirect:/";
     }
 }
