dcadfb2b354ddcf629422bfb2ea430bfe3c91afd
w0nnie <kjw55523@gmail.com>
2023-06-08 11:56:19 +0900

    스프링 통합 테스트

--- hello-spring2/src/test/java/study/hellospring2/service/MemberServiceIntegrationTest.java
+++ hello-spring2/src/test/java/study/hellospring2/service/MemberServiceIntegrationTest.java
@@ -3,32 +3,25 @@ package study.hellospring2.service;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import study.hellospring2.domain.Member;
+import study.hellospring2.repository.MemberRepository;
 import study.hellospring2.repository.MemoryMemberRepository;
 
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-/**
- * 순수한 단위테스트 (JVM 내에서 가능한 테스트)가 훨씬더 좋은 테스트 일 확률이 높다.
- * spring 컨테이너 없이 순수한 java로 이루어진 단위 테스트 환경에서 테스트하는법을 더 공부하자.
- */
-class MemberServiceTest {
+@SpringBootTest // :  스프링 컨테이너와 테스트를 함께 실행한다
+@Transactional //  : 테스트 케이스에 이 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고,  테스트 완료 후에 항상 롤백한다. 이렇게 하면 DB에 데이터가 남지 않으므로 다음 테스트에 영향을 주지않는다.
+class MemberServiceIntegrationTest {
 
+    @Autowired
     MemberService service;
-    MemoryMemberRepository repository;
 
-    @BeforeEach
-    void beforeEach() {
-        repository = new MemoryMemberRepository();
-        service = new MemberService(repository);
-    }
-
-    @AfterEach
-    void afterEach() {
-        repository.clearStore();
-    }
+    @Autowired
+    MemberRepository repository;
 
     @Test
     void 회원가입() {
