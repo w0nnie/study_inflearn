3ec925c11aac2278db4db0e6b192a813ff5e1113
w0nnie <kjw55523@gmail.com>
2023-06-12 16:15:26 +0900

    fixed untracked files

--- hello-spring2/src/main/java/SpringConfig.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import study.hellospring2.repository.MemberRepository;
-
-@Configuration
-public class SpringConfig {
-
-}
