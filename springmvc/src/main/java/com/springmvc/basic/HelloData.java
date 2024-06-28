bd3c7d5c5383572391318c682d4d2230e58eda5d
w0nnie <kjw55523@gmail.com>
2024-06-28 14:30:35 +0900

    [섹션6] HTTP 요청 메시지 - 단순 텍스트

--- springmvc/src/main/java/com/springmvc/basic/HelloData.java
+++ springmvc/src/main/java/com/springmvc/basic/HelloData.java
@@ -1,10 +1,8 @@
 package com.springmvc.basic;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class HelloData {
     private String username;
     private int age;
