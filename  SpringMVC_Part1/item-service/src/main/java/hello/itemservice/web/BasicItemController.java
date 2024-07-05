8f9bfdad9d4f1d905135bd9c902bf3398bba9d2f
w0nnie <kjw55523@gmail.com>
2024-07-05 09:32:02 +0900

    [섹션7] 상품 상세

---  SpringMVC_Part1/item-service/src/main/java/hello/itemservice/web/BasicItemController.java
+++  SpringMVC_Part1/item-service/src/main/java/hello/itemservice/web/BasicItemController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,6 +33,18 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping("{itemId}")
+    public String itemDetail(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
     /**
      * 테스트용 데이터 추가
      * 의존성 주입이 완료된 후에 실행되어야 하는 method에 사용
@@ -40,6 +53,7 @@ public class BasicItemController {
      */
     @PostConstruct
     public void init() {
+
         itemRepository.save(new Item("testA", 10000, 10));
         itemRepository.save(new Item("testB", 20000, 20));
     }
