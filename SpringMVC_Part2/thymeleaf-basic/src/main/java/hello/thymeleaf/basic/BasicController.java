96afc3c5614b44fee468d0c8d545b081bd6b1284
w0nnie <kjw55523@gmail.com>
2024-07-08 14:47:53 +0900

    [타임리프-기본 기능] 기본 객체들, 유틸리티 객체와 날짜

--- SpringMVC_Part2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ SpringMVC_Part2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,12 +1,17 @@
 package hello.thymeleaf.basic;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +20,29 @@ import java.util.Map;
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
 
     @GetMapping("/text-basic")
     public String textBasic(Model model) {
