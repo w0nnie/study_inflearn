2b7db839e8f4cb2ccfd9cf236c20f67390cbb646
w0nnie <kjw55523@gmail.com>
2024-07-08 15:11:15 +0900

    [타임리프-기본 기능] URL 링크

--- SpringMVC_Part2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ SpringMVC_Part2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -21,6 +21,14 @@ import java.util.Map;
 @RequestMapping("/basic")
 public class BasicController {
 
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @GetMapping("/date")
     public String date(Model model) {
         model.addAttribute("localDateTime", LocalDateTime.now());
