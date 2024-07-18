b059e68ae39acaba3aa07bc20cb7d87eda103a07
jinwon Kwon <kjw55523@gmail.com>
2024-07-18 21:59:50 +0900

    [타임리프 - 스프링 통합과 폼] 셀렉트 박스

--- SpringMVC_Part2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ SpringMVC_Part2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +24,19 @@ public class FormItemController {
 
     private final ItemRepository itemRepository;
 
+    public static List<DeliveryCode> getDeliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+
+        return deliveryCodes;
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        return getDeliveryCodes();
+    }
 
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
